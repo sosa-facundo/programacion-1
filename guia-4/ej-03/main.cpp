f30a07cb7e57f7e98a50a49152fc39103aa0ea08
Facundo <sosafacundo.94@gmail.com>
2025-02-25 10:20:57 -0300

    Ejercicio 3 GUIA WHILE 4 (Ciclo inexacto)

---
+++ guia-4/ej-03/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/**Hacer un programa para mostrar por pantalla los n£meros del 20 al 1 en orden
+decreciente.
+Importante: El programa no tiene ning£n ingreso de datos.
+*/
+int main (){
+    int numero=20;
+
+    while(numero>0)
+    {
+        cout<<numero<<endl;
+        numero=numero-1;
+    }
+
+return 0;
+}
