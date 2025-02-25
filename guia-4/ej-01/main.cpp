373727e878764c8381c9166a09f003e4c2d49a99
Facundo <sosafacundo.94@gmail.com>
2025-02-25 10:06:29 -0300

    Ejercicio 1 GUIA WHILE 4 (Ciclo inexacto)

---
+++ guia-4/ej-01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/**Hacer un programa para mostrar por pantalla los n£meros del 1 al 10.
+Importante: El programa no tiene ning£n ingreso de datos.
+*/
+int main(){
+    int num=0;
+    while (num<10)
+    {
+        num++;
+        cout<<num<<endl;
+    }
+return 0;
+}
