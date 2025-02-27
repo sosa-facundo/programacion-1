80b0e767a84da16386e37126655eac8fd945195a
Facundo <sosafacundo.94@gmail.com>
2025-02-27 11:21:19 -0300

    Ejercicio 6 GUIA WHILE 4 (Ciclo inexacto)

---
+++ guia-4/ej-06/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+/** Hacer un programa para ingresar una lista de n£meros que finaliza cuando se
+ingresa un cero, luego informar cu ntos son positivos y cu ntos son negativos.
+Ejemplo: 4, -3, 8, -5, 18, 20, 0. Se listar  Positivos: 4 Negativos: 2.
+Para resolver este ejercicio sugerimos resolver antes el TP3 EJ 7*/
+int main(){
+    int numero;
+    int contadorPositivo=0;
+
+    do
+    {
+        cin>>numero;
+        if(numero>0)
+        {
+            contadorPositivo=contadorPositivo+1;
+        }
+    } while(numero!=0);
+    cout<<"son = "<<contadorPositivo<<" numeros positivos.";
+return 0;
+}
